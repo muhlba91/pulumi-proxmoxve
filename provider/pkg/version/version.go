ed93068a5aeb46fc92056008f005f8bb0904f11a
Daniel Muehlbachler-Pietrzykowski <daniel@muehlbachler.io>
2026-02-17 08:53:23 +0100

    fix: fix golangci revive

--- provider/pkg/version/version.go
+++ provider/pkg/version/version.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//nolint:revive // package name is fine as is
 package version
 
 // Version is initialized by the Go linker to contain the semver of this build.
